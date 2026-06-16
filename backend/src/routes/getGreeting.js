760ea5162893b47906bb631da192c142b20e99dc
dario-aliprandi-sdg <dario.aliprandi@sdggroup.com>
2026-06-16 11:31:38 +0200

    Modify getGreeting to randomize messages

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,7 +1,13 @@
-const GREETING = 'Hello world!';
+const GREETING = ['Hello Caterina', 'Hello Dario', 'Hello Amaia' ];
 
 module.exports = async (req, res) => {
     res.send({
         greeting: GREETING,
     });
+};
+
+module.exports = async (req, res) => {
+  res.send({
+    greeting: GREETINGS[Math.floor(Math.random() * GREETINGS.length)],
+  });
 };
